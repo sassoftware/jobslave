f1d767084b664905eb395f5520cf4e182e6bd8aa
Stu Gott <http://issues.rpath.com/>
2007-01-26 11:30:50 -0500

    tweak setup process

--- setup.py
+++ setup.py
@@ -4,11 +4,15 @@
 # All rights reserved
 #
 
-# manifest is a list of relative paths to include in teh data tarball
-manifest = ['templates', 'skel', 'pixmaps']
+# dataManifest is a list of relative paths to include in the data tarball
+dataManifest = ['templates', 'skel', 'pixmaps']
 
-import os
+import os, sys
+
+# force production of source distribution only.
+sys.argv = [sys.argv[0], 'sdist']
 from jobslave import constants
+from conary.lib import util
 
 # do standard setuptools inclusion
 from setuptools import setup, find_packages
@@ -20,4 +24,10 @@ setup(
 
 # include data separately to avoid complicated setuptools rules
 os.system('tar -czvO %s > "dist/jobslave-data-%s.tar.gz"' % \
-              ('"' + '" "'.join(manifest) + '"', constants.version))
+              ('"' + '" "'.join(dataManifest) + '"', constants.version))
+
+# force deletion of .egg-info dirs
+for eggDir in [x for x in \
+                   os.listdir(os.path.split(os.path.abspath(__file__))[0]) \
+                   if x.endswith('.egg-info')]:
+    util.rmtree(os.path.abspath(eggDir))
