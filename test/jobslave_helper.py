60cab8fef638b210827f4c9729eedc2c9eae0a7a
Stu Gott <http://issues.rpath.com/>
2007-02-01 10:34:16 -0500

    added helper code

--- test/jobslave_helper.py
+++ test/jobslave_helper.py
@@ -5,7 +5,9 @@
 # All rights reserved
 #
 
+import os, sys
 import testhelp
+from jobslave import jobhandler
 
 class DummyConnection(object):
     def __init__(self, *args, **kwargs):
@@ -50,5 +52,37 @@ class DummyConnection(object):
     def disconnect(self):
         pass
 
+class DummyResponse(object):
+    response = DummyConnection()
+
 class JobSlaveHelper(testhelp.TestCase):
-    pass
+    def getHandler(self, buildType):
+        return jobhandler.getHandler( \
+            {'serialVersion': 1,
+             'type' : 'build',
+             'project' : {'name': 'Foo',
+                          'hostname' : 'foo',
+                          'label': 'foo.rpath.local@rpl:devel',
+                          'conaryCfg': ''},
+             'name' : 'Foo',
+             'UUID' : 'mint.rpath.local-build-25',
+             'troveName' : 'group-core',
+             'troveVersion' : '/conary.rpath.com@rpl:1/0:1.0.1-1-1',
+             'troveFlavor': '1#x86',
+             'data' : {'jsversion': '3.0.0'},
+             'outputQueue': 'test',
+             'buildType' : buildType},
+            DummyResponse())
+
+    def supressOutput(self, func, *args, **kwargs):
+        oldErr = os.dup(sys.stderr.fileno())
+        oldOut = os.dup(sys.stdout.fileno())
+        fd = os.open(os.devnull, os.W_OK)
+        os.dup2(fd, sys.stderr.fileno())
+        os.dup2(fd, sys.stdout.fileno())
+        os.close(fd)
+        try:
+            return func(*args, **kwargs)
+        finally:
+            os.dup2(oldErr, sys.stderr.fileno())
+            os.dup2(oldOut, sys.stdout.fileno())
