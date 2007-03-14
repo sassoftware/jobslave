70586be9b041524e6b122dd59578eef8e125df5e
Tim Gerla <http://issues.rpath.com/>
2007-03-14 15:58:43 -0400

    more robust /etc/localtime handling

--- jobslave/generators/bootable_image.py
+++ jobslave/generators/bootable_image.py
@@ -292,9 +292,9 @@ class BootableImage(ImageGenerator):
             util.execute(cmd)
 
         # copy timezone data into /etc/localtime
-        if not os.path.exists(os.path.join(fakeRoot, 'etc', 'localtime')):
-            os.copy(os.path.join('usr', 'share', 'zoneinfo', 'UTC'),
-                    os.path.join('etc', 'localtime'))
+        if os.path.exists(os.path.join(self.fakeroot, 'usr', 'share', 'zoneinfo', 'UTC')):
+            copyfile(os.path.join(self.fakeroot, 'usr', 'share', 'zoneinfo', 'UTC'),
+                     os.path.join(self.fakeroot, 'etc', 'localtime'))
 
     def __init__(self, *args, **kwargs):
         ImageGenerator.__init__(self, *args, **kwargs)
