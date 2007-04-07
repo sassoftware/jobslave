76c366f845b483a869f35b0452e1ae5d83af377b
Tim Gerla <http://issues.rpath.com/>
2007-04-07 11:26:23 -0400

    add support for job requests lacking filesystem info

--- jobslave/generators/bootable_image.py
+++ jobslave/generators/bootable_image.py
@@ -235,6 +235,16 @@ class BootableImage(ImageGenerator):
         ImageGenerator.__init__(self, *args, **kwargs)
         self.scsiModules = False
 
+        if filesystems not in self.jobData:
+            # support for legacy requests
+            freeSpace = self.getBuildData("freespace") * 1048576
+            swapSize = self.getBuildData("swapSize") * 1048576
+
+            self.jobData['filesystems'] = [
+                ('/', 0, freeSpace, 'ext3'),
+                ('swap', 0, swapSize, 'swap'),
+            ]
+
         log.info('building trove: (%s, %s, %s)' % \
                  (self.baseTrove, self.baseVersion, str(self.baseFlavor)))
 
