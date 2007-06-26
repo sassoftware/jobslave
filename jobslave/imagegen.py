f933a0ce1b0f1c5ce9d121c20dfd7336ec9bcca9
Tim Gerla <http://issues.rpath.com/>
2007-06-26 14:28:34 -0400

    no need to create this directory here

--- jobslave/imagegen.py
+++ jobslave/imagegen.py
@@ -231,7 +231,6 @@ class Generator(threading.Thread):
                     os.close(inF)
                     self.parentPipe = outF
                     self.status('Starting job')
-                    util.mkdirChain(constants.finishedDir)
                     self.write()
                     try:
                         if self.workDir and os.path.exists(self.workDir):
