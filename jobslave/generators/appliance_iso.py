d7eccabd1d378a4689da3ead1d7383ae292aaca5
Elliot Peele <http://bugs.rpath.com/>
2007-07-06 11:30:35 -0400

    clean up whitespace use productDir for productPackagePath

--- jobslave/generators/appliance_iso.py
+++ jobslave/generators/appliance_iso.py
@@ -57,7 +57,7 @@ class TarSplit(object):
     def writeTbList(self, file):
         fh = open(file, 'w')
         fh.write('\n'.join(self.tblist))
-	fh.write('\n')
+        fh.write('\n')
         fh.close()
 
 class ApplianceInstaller(bootable_image.BootableImage, 
@@ -66,7 +66,7 @@ class ApplianceInstaller(bootable_image.BootableImage,
         bootable_image.BootableImage.__init__(self, *args, **kwargs)
         self.showMediaCheck = self.getBuildData('showMediaCheck')
         #self.maxIsoSize = int(self.getBuildData('maxIsoSize'))
-	self.maxIsoSize = 0
+        self.maxIsoSize = 0
 
         if 'filesystems' not in self.jobData:
             # support for legacy requests
@@ -91,11 +91,11 @@ class ApplianceInstaller(bootable_image.BootableImage,
                            'arch': self.arch}
 
         self.basefilename = basefilename
-    
+
     def writeBuildStamp(self, tmpPath):
         installable_iso.InstallableIso.writeBuildStamp(self, tmpPath)
         bsFile = open(os.path.join(tmpPath, ".buildstamp"), "a")
-        print >> bsFile, 'rPath/tarballs'
+        print >> bsFile, '%s/tarballs' % self.productDir
         bsFile.close()
 
     def write(self):
@@ -130,26 +130,26 @@ class ApplianceInstaller(bootable_image.BootableImage,
                 # block all errors so that real ones can get through
                 pass
 
-	    self._setupTrove()
-	    self.callback = installable_iso.Callback(self.status)
+            self._setupTrove()
+            self.callback = installable_iso.Callback(self.status)
 
             print >> sys.stderr, "Building ISOs of size: %d Mb" % \
               (self.maxIsoSize / 1048576)
             sys.stderr.flush()
-    
+
             # FIXME: hack to ensure we don't trigger overburns.
             # there are probably cleaner ways to do this.
             if self.maxIsoSize > 681574400:
                 self.maxIsoSize -= 1024 * 1024
-    
+
             csdir = self.prepareTemplates(topDir)
             util.rmtree(csdir, ignore_errors=True)
-    
+
             if self.arch == 'x86':
                 anacondaArch = 'i386'
             else:
                 anacondaArch = self.arch
-    
+
             # write .discinfo
             discInfoPath = os.path.join(topDir, ".discinfo")
             if os.path.exists(discInfoPath):
@@ -162,16 +162,16 @@ class ApplianceInstaller(bootable_image.BootableImage,
             for x in ["base", "tarballs", 'pixmaps']:
                 print >> discInfoFile, "%s/%s" % (self.productDir, x)
             discInfoFile.close()
-    
+
             self.extractMediaTemplate(topDir)
             self.setupKickstart(topDir)
             self.writeProductImage(topDir, installable_iso.getArchFlavor(self.baseFlavor).freeze())
-    
+
             self.status("Building ISOs")
-	    
-	    # Mostly copied from splitdistro
+
+            # Mostly copied from splitdistro
             current = os.path.join(constants.tmpDir, self.jobId, 'disc1')
-    	    discnum = 1
+            discnum = 1
             if os.path.isdir(current):
                 print >> sys.stderr, 'removing stale', current
                 util.rmtree(current)
@@ -183,7 +183,7 @@ class ApplianceInstaller(bootable_image.BootableImage,
                 if 'media-template' in os.listdir(topDir) and \
                        cDir in os.listdir(os.path.join(topDir, 'media-template')):
                     splitdistro.lndir(os.path.join(topDir, 'media-template', cDir), current)
-        
+
             for cDir in ('all', 'disc1'):
                 srcDir = os.path.join(topDir, 'media-template2', cDir)
                 if os.path.exists(srcDir):
@@ -191,9 +191,8 @@ class ApplianceInstaller(bootable_image.BootableImage,
                         call('cp', '-R', '--no-dereference', os.path.join(srcDir, src),
                              current)
 
-	    
             isoList = self.buildIsos(topDir)
-    
+
             # notify client that images are ready
             self.postOutput(isoList)
         finally:
