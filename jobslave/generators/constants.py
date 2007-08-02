edc43251420aa9ec2a21632c53f8f54e25e53006
Stu Gott <http://issues.rpath.com/>
2007-08-02 15:51:54 -0400

    move /srv/jobslave/tmp back to /tmp. it's not NFS anymore.

--- jobslave/generators/constants.py
+++ jobslave/generators/constants.py
@@ -20,7 +20,7 @@ partitionOffset = 65536
 import os
 
 # path for temporary finished images
-finishedDir = os.path.join(os.path.sep, 'srv', 'jobslave', 'tmp', 'finished-images')
+finishedDir = os.path.join(os.path.sep, 'tmp', 'finished-images')
 
 # directory containing file tree with fstab and other hooks
 skelDir         = os.path.join(os.path.sep, 'srv', 'jobslave', 'skel')
@@ -32,12 +32,12 @@ fallbackDir     = os.path.join(os.path.sep, 'srv', 'jobslave', 'fallback')
 templateDir = os.path.join(os.path.sep, 'srv', 'jobslave', 'templates')
 
 # temporary directory
-tmpDir = os.path.join(os.path.sep, 'srv', 'jobslave', 'tmp')
+tmpDir = os.path.join(os.path.sep, 'tmp')
 
 scriptPath = os.path.join(os.path.sep, 'usr', 'share', 'jobslave', 'scripts')
-cachePath = os.path.join(os.path.sep, 'srv', 'jobslave', 'tmp', 'changesets')
+cachePath = os.path.join(os.path.sep, 'tmp', 'changesets')
 implantIsoMd5 = os.path.join(os.path.sep, 'usr', 'bin', 'implantisomd5')
 anacondaImagesPath = os.path.join(os.path.sep, 'srv', 'jobslave', 'pixmaps')
-anacondaTemplatesPath = os.path.join(os.path.sep, 'srv', 'jobslave', 'tmp', 'anaconda-templates')
+anacondaTemplatesPath = os.path.join(os.path.sep, 'tmp', 'anaconda-templates')
 templatesLabel = 'conary.rpath.com@rpl:1'
 entDir =  os.path.join(os.path.sep, 'srv', 'jobslave', 'entitlements')
