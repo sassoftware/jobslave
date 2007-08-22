8ae64a19e480780c200c5730312aeec4365cc81c
Tim Gerla <http://issues.rpath.com/>
2007-08-22 15:40:07 -0400

    revert last

--- jobslave/slave.py
+++ jobslave/slave.py
@@ -228,7 +228,7 @@ class JobSlave(object):
 
             sha1 = sha.new()
             inF = open(fn)
-            httpPutFile(url, inF, size, chunked = False,
+            httpPutFile(url, inF, size, chunked = True,
                 extraHeaders = [('X-rBuilder-OutputToken', outputToken)],
                 digest = sha1)
 
