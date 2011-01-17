51055b6fbd81ffc88215b88f9a63d702535bdd93
Michael Tharp <mtharp@rpath.com>
2011-01-17 10:41:23 -0500

    Add missing file

---
+++ jobslave/job_data.py
@@ -0,0 +1,42 @@
+#
+# Copyright (c) 2011 rPath, Inc.
+#
+
+
+BUILD_DEFAULTS = {
+        'autoResolve': False,
+        'maxIsoSize': '681574400',
+        'bugsUrl': 'http://issues.rpath.com/',
+        'natNetworking': False,
+        'vhdDiskType': 'dynamic',
+        'anacondaCustomTrove': '',
+        'stringArg': '',
+        'mediaTemplateTrove': '',
+        'baseFileName': '',
+        'vmSnapshots': False,
+        'swapSize': 128,
+        'betaNag': False,
+        'anacondaTemplatesTrove': '',
+        'enumArg': '2',
+        'vmMemory': 256,
+        'installLabelPath': '',
+        'intArg': 0,
+        'freespace': 250,
+        'boolArg': False,
+        'mirrorUrl': '',
+        'zisofs': True,
+        'diskAdapter': 'lsilogic',
+        'unionfs': False,
+        'showMediaCheck': False,
+        'amiHugeDiskMountpoint': '',
+        'platformName': '',
+        }
+
+
+class JobData(dict):
+
+    def getBuildData(self, key):
+        value = self.get('data', {}).get(key)
+        if value is None:
+            value = BUILD_DEFAULTS.get(key)
+        return value
