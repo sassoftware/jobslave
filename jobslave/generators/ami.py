e7642930d09f9d2f08d8faa81daa0d447d550d0d
Michael Tharp <michael.tharp@sas.com>
2015-05-27 19:09:30 +0000

    Relicense as Apache 2 (APPENG-3585)

--- jobslave/generators/ami.py
+++ jobslave/generators/ami.py
@@ -1,8 +1,19 @@
 #
-# Copyright (c) 2010-2012 rPath, Inc.
+# Copyright (c) SAS Institute Inc.
 #
-# All Rights Reserved
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 
 import logging
 log = logging.getLogger(__name__)
@@ -37,4 +48,3 @@ class AMIImage(raw_hd_image.RawHdImage):
                     freeSpace=freeSpace),
                 ]
         return dict((x.mount, x) for x in fsList)
-
